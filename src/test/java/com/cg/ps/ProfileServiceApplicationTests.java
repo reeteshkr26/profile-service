b6bd4fbdf2dcaf8ccc09de28c648dd6e67f1239f
Reetesh_Ranjan <reeteshkumarmandal@gmail.com>
2020-11-18 02:45:33 +0530

    add logger in test case class

--- src/test/java/com/cg/ps/ProfileServiceApplicationTests.java
+++ src/test/java/com/cg/ps/ProfileServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cg.ps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProfileServiceApplicationTests {
 
+	public static Logger logger=LoggerFactory.getLogger(ProfileServiceApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing..");
+		assertEquals(true, true);
 	}
 
 }
